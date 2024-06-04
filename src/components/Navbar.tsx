c18f839ebc4e5ce91188b98688e01edfe742e9c7
Muhammad Shariq <muhammadshariq368@gmail.com>
2024-06-04 09:53:41 +0500

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,28 @@
 import Image from "next/image"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import Logo from "../../public/picture/logo-p.png"
+import Img from "../../public/picture/image.png"
+
 
 
 const Navbar = () => {
   return (
     <div>
-         <div className="border-b">
-          <div className="flex h-16 items-center px-4 container mx-auto">
-            <Image src={Logo} width={300} height={300} alt="logo"
-            className="w-[7rem]"/>
-          
+      <div className="border-b">
+        <div className="flex h-16 items-center px-4 container mx-auto">
+          <Image src={Logo} width={300} height={300} alt="logo"
+            className="w-[7rem]" />
+
           <div className="ml-auto flex items-center space-x-4">
-          <Avatar>
-  <AvatarImage src='' />
-  <AvatarFallback>MS</AvatarFallback>
-</Avatar>
+            <Avatar>
+              <Image src={Img} width={500} height={500} alt="image" 
+              />
+              <AvatarFallback>MS</AvatarFallback>
+            </Avatar>
 
           </div>
-            </div>
-            </div>
+        </div>
+      </div>
     </div>
   )
 }
